825b369edfa31ec457cd279c7c1fb048bffbcb60
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-07-26 19:37:49 +0300

    переформатирование

--- WoodenCompass/main.cpp
+++ WoodenCompass/main.cpp
@@ -7,21 +7,17 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	QApplication app{argc, argv};
-	try
-	{
-		cout << "Wooden Compass" << endl;
+int main(int argc, char **argv) {
+    QApplication app { argc, argv };
+    try {
+        cout << "Wooden Compass" << endl;
 
-		QPushButton button{"Test button"};
-		QObject::connect(&button, &QPushButton::clicked, &app, &QApplication::quit);
-		button.show();
+        QPushButton button { "Test button" };
+        QObject::connect(&button, &QPushButton::clicked, &app, &QApplication::quit);
+        button.show();
 
-		return app.exec();
-	}
-	catch (exception const& e)
-	{
-		QMessageBox::critical(nullptr, "Error", e.what());
-	}
+        return app.exec();
+    } catch (exception const& e) {
+        QMessageBox::critical(nullptr, "Error", e.what());
+    }
 }
